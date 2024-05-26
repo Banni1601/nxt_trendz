8527729aaca90e437b5cf5c2b4fd9cec68fe90a9
Banni Babu Veedhi <138286411+Banni1601@users.noreply.github.com>
2024-05-26 18:17:05 +0530

    Update index.js

--- src/components/SimilarProductItem/index.js
+++ src/components/SimilarProductItem/index.js
@@ -1,31 +1,36 @@
 import './index.css'
+import React from 'react'
+import {Link} from 'react-router-dom'
 
 const SimilarProductItem = props => {
   const {productDetails} = props
-  const {title, brand, imageUrl, rating, price} = productDetails
+  const {title, brand, imageUrl, rating, price, id} = productDetails
 
   return (
-    <li className="similar-product-item">
-      <img
-        src={imageUrl}
-        className="similar-product-img"
-        alt={`similar product ${title}`}
-      />
-      <p className="similar-product-title">{title}</p>
-      <p className="similar-products-brand">by {brand}</p>
-      <div className="similar-product-price-rating-container">
-        <p className="similar-product-price">Rs {price}/-</p>
-        <div className="similar-product-rating-container">
-          <p className="similar-product-rating">{rating}</p>
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/star-img.png"
-            alt="star"
-            className="similar-product-star"
-          />
+    <Link to={`/products/${id}`} className="styless">
+      <li className="similar-product-item">
+        <img
+          src={imageUrl}
+          className="similar-product-img"
+          alt={`similar product ${title}`}
+        />
+        <p className="similar-product-title">{title}</p>
+        <p className="similar-products-brand">by {brand}</p>
+        <div className="similar-product-price-rating-container">
+          <p className="similar-product-price">Rs {price}/-</p>
+          <div className="similar-product-rating-container">
+            <p className="similar-product-rating">{rating}</p>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+              alt="star"
+              className="similar-product-star"
+            />
+          </div>
         </div>
-      </div>
-    </li>
+      </li>
+    </Link>
   )
 }
 
 export default SimilarProductItem
+
